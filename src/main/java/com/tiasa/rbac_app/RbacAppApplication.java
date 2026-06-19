58a975585390f532f758d295167d1c121058be9b
gunturachievement <guntur.achievement@gmail.com>
2026-06-19 20:03:55 +0700

    first-commit(structure project, entity role and users)

---
+++ src/main/java/com/tiasa/rbac_app/RbacAppApplication.java
@@ -0,0 +1,13 @@
+package com.tiasa.rbac_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbacAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbacAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/tiasa/rbac_app/entity/User.java
@@ -0,0 +1,46 @@
+package com.tiasa.rbac_app.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String fullName;
+
+    @Email
+    @NotBlank
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String password; // akan di-hash dengan BCrypt nanti
+
+    private boolean enabled = true;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    @Builder.Default
+    private Set<Role> roles = new HashSet<>();
+}
